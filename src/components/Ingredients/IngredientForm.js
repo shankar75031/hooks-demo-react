8735e42bd98e4faa601ad55843fd0e7502363593
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-21 20:44:12 +0530

    split state to multiple pieces to avoid merging while setting state

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,10 +4,8 @@ import Card from "../UI/Card";
 import "./IngredientForm.css";
 
 const IngredientForm = React.memo((props) => {
-  const [inputState, setInputState] = useState({
-    title: "",
-    amount: "",
-  });
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
 
   const submitHandler = (event) => {
     event.preventDefault();
@@ -22,13 +20,10 @@ const IngredientForm = React.memo((props) => {
             <input
               type="text"
               id="title"
-              value={inputState.title}
+              value={enteredTitle}
               onChange={(event) => {
                 const newTitle = event.target.value;
-                setInputState((prevState) => ({
-                  title: newTitle,
-                  amount: prevState.amount,
-                }));
+                setEnteredTitle(newTitle);
               }}
             />
           </div>
@@ -37,13 +32,10 @@ const IngredientForm = React.memo((props) => {
             <input
               type="number"
               id="amount"
-              value={inputState.amount}
+              value={enteredAmount}
               onChange={(event) => {
                 const newAmount = event.target.value;
-                setInputState((prevState) => ({
-                  amount: newAmount,
-                  title: prevState.title,
-                }));
+                setEnteredAmount(newAmount);
               }}
             />
           </div>
