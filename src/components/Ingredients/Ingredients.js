87089a0940b1bb5c9ab58a670445753a17a414f3
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-21 23:50:03 +0530

    fetch ingredients

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -6,6 +6,31 @@ import Search from "./Search";
 
 const Ingredients = (props) => {
   const [userIngredients, setUserIngredients] = useState([]);
+
+  // Gets executed whenever a component has finished rendering
+  // Second arguement:
+  // 1. If empty array it runs only once after the component is first rendered
+  // 2. If any variable(s) then first function runs when ever the variable(s) change
+  useEffect(() => {
+    fetch(
+      "https://react-hooks-demo-ad70f-default-rtdb.firebaseio.com/ingredients.json"
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   const addIngredientHandler = (ingredient) => {
     fetch(
