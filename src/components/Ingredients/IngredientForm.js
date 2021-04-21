4490f3167579ab10d319cf241847580e9ba93265
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-21 20:19:56 +0530

    added binding for form input elements to  state

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import "./IngredientForm.css";
 
 const IngredientForm = React.memo((props) => {
+  const inputState = useState({
+    title: "",
+    amount: "",
+  });
+
   const submitHandler = (event) => {
     event.preventDefault();
-    // ...
   };
 
   return (
@@ -15,11 +19,33 @@ const IngredientForm = React.memo((props) => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input
+              type="text"
+              id="title"
+              value={inputState[0].title}
+              onChange={(event) => {
+                const newTitle = event.target.value;
+                inputState[1]((prevState) => ({
+                  title: newTitle,
+                  amount: prevState.amount,
+                }));
+              }}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input
+              type="number"
+              id="amount"
+              value={inputState[0].amount}
+              onChange={(event) => {
+                const newAmount = event.target.value;
+                inputState[1]((prevState) => ({
+                  amount: newAmount,
+                  title: prevState.title,
+                }));
+              }}
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
