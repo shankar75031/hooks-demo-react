049e7e43b9fb3057daa1587dc88f3252c0658944
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-22 00:24:42 +0530

    added timer for search init

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
@@ -6,38 +6,42 @@ import "./Search.css";
 const Search = React.memo((props) => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState("");
+  const inputRef = useRef();
 
   // Gets executed whenever a component has finished rendering
   // Second arguement:
   // 1. If empty array it runs only once after the component is first rendered
   // 2. If any variable(s) then first function runs when ever the variable(s) change
   useEffect(() => {
-    setTimeout(() => {}, 500);
-    const queryParams =
-      enteredFilter.length === 0
-        ? ""
-        : `?orderBy=\"title\"&equalTo=\"${enteredFilter}\"`;
-    fetch(
-      "https://react-hooks-demo-ad70f-default-rtdb.firebaseio.com/ingredients.json" +
-        queryParams
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then((responseData) => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
-          });
-        }
-        //Trigger something in Ingredients
-        onLoadIngredients(loadedIngredients);
-      })
-      .catch((err) => console.log(err));
-  }, [enteredFilter, onLoadIngredients]);
+    setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const queryParams =
+          enteredFilter.length === 0
+            ? ""
+            : `?orderBy=\"title\"&equalTo=\"${enteredFilter}\"`;
+        fetch(
+          "https://react-hooks-demo-ad70f-default-rtdb.firebaseio.com/ingredients.json" +
+            queryParams
+        )
+          .then((res) => {
+            return res.json();
+          })
+          .then((responseData) => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              });
+            }
+            //Trigger something in Ingredients
+            onLoadIngredients(loadedIngredients);
+          })
+          .catch((err) => console.log(err));
+      }
+    }, 500);
+  }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
     <section className="search">
@@ -45,6 +49,7 @@ const Search = React.memo((props) => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             value={enteredFilter}
             onChange={(event) => {
